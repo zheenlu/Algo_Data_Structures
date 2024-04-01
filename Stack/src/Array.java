e13465d5c9404d7f52668e87fbbcbbce728f90ee
zheenlu <lz.zhenlu@gmail.com>
2024-04-01 09:56:13 -0400

    1.3.2.2 build stack

--- Stack/src/Array.java
+++ Stack/src/Array.java
@@ -74,6 +74,13 @@ public class Array<E> {
         return data[index];
     }
 
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
     public void set(int index, E e) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("Set failed. Index is illegal");
