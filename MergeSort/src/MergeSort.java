ff9d790103a2ea6ae634204edb901cc8b65706db
zheenlu <lz.zhenlu@gmail.com>
2024-04-13 14:58:49 -0400

    2.1.1-7 在程序上调试跟踪归并排序法

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -1,63 +1,79 @@
 import java.util.Arrays;
 
 public class MergeSort {
-    private MergeSort(){};
 
-    public static <E extends Comparable<E>> void sort(E[] arr) {
-        sort(arr, 0, arr.length - 1);
+    private MergeSort(){}
+
+    public static <E extends Comparable<E>> void sort(E[] arr){
+
+        sort(arr, 0, arr.length - 1, 0);
     }
 
-    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r) {
-        // 没有元素或只有一个元素情况
-        if (l >= r) {
+    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r, int depth){
+
+        // 生成深度字符串
+        String depthString = generateDepthString(depth);
+
+        // 打印当前 sort 处理的数组区间信息
+        System.out.print(depthString);
+        System.out.println(String.format("mergesort arr[%d, %d]", l, r));
+
+        if (l >= r)
             return;
-        }
 
         int mid = l + (r - l) / 2;
-        sort(arr, l, mid);
-        sort(arr, mid + 1, r);
+        sort(arr, l, mid, depth + 1);
+        sort(arr, mid + 1, r, depth + 1);
+
+        // 打印这次 merge 要处理的区间范围
+        System.out.print(depthString);
+        System.out.println(String.format("merge arr[%d, %d] and arr[%d, %d]", l, mid, mid + 1, r));
+
         merge(arr, l, mid, r);
+
+        // 打印 merge 后的数组
+        System.out.print(depthString);
+        System.out.print(String.format("after mergesort arr[%d, %d] :", l, r));
+        for(E e: arr)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    private static String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0 ; i < depth ; i ++)
+            res.append("--");
+        return res.toString();
     }
 
 
-    /**
-     * 合并两个有序的区间 arr[l, mid] 和 arr[mid + 1, r]
-     * @param arr
-     * @param l
-     * @param mid
-     * @param r
-     * @param <E>
-     */
-    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
-        E[] temp = Arrays.copyOfRange(arr, l, r + 1); // 左闭右开所以 r + 1
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r){
+
+        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
 
         int i = l, j = mid + 1;
 
         // 每轮循环为 arr[k] 赋值
-        for (int k = l; k <= r; k++) {
-            if (i > mid) { // 左边区间看完了
-                // arr[k] 这时候应该等于arr[j]里取的值
-                // 但不能直接写arr[j]，因为可能以及被覆盖了
-                // arr[i] 是temp[0], arr[i+1] 是temp[1]
-                // 因为有偏移，如果要想得到j位置的值，所以写成j-l
-                arr[k] = temp[j - l];
-                j++;
-            } else if (j > r) { // 右边元素看完了
-                arr[k] = temp[i- l];
-                i++;
-            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
-                arr[k] = temp[i - l];
-                i++;
-            } else {
-                arr[k] = temp[j - l];
-                j++;
+        for(int k = l; k <= r; k ++){
+
+            if(i > mid){
+                arr[k] = temp[j - l]; j ++;
+            }
+            else if(j > r){
+                arr[k] = temp[i - l]; i ++;
+            }
+            else if(temp[i - l].compareTo(temp[j - l]) <= 0){
+                arr[k] = temp[i - l]; i ++;
+            }
+            else{
+                arr[k] = temp[j - l]; j ++;
             }
         }
     }
 
-    public static void main(String[] args) {
-        int n = 100000;
-        Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+    public static void main(String[] args){
+
+        Integer[] arr = {7, 1, 4, 2, 8, 3, 6, 5};
         SortingHelper.sortTest("MergeSort", arr);
     }
 }
