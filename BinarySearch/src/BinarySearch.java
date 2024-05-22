7063189170ee385f1951559516743ebf14d5b62c
zheenlu <lz.zhenlu@gmail.com>
2024-05-22 08:42:29 -0400

    2.3.1-3 二分查找法的非递归写法

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -1,11 +1,44 @@
 public class BinarySearch {
     private BinarySearch(){};
 
+    /**
+     * 非递归实现二分查找法
+     * @param data
+     * @param target
+     * @return
+     * @param <E>
+     */
     public static <E extends Comparable<E>> int search(E[] data, E target) {
-        return search(data, 0, data.length - 1, target);
+        int  l = 0, r = data.length - 1;
+        // 在 data[l, r] 的范围中查找target
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) == 0) {
+                return mid;
+            }
+            if (data[mid].compareTo(target) < 0) {
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return -1;
     }
 
-    private static <E extends Comparable<E>> int search(E[] data, int l, int r, E target) {
+
+    /**
+     * 递归是爱你二分查找法
+     * @param data
+     * @param target
+     * @return
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> int searchR(E[] data, E target) {
+        return searchR(data, 0, data.length - 1, target);
+    }
+
+    private static <E extends Comparable<E>> int searchR(E[] data, int l, int r, E target) {
         if (l < r) return -1;
 
         int mid = l + (r - l) / 2;
