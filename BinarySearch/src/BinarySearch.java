7f76688481d3f49f9c4ff28e57740d7480ffc2ed
zheenlu <lz.zhenlu@gmail.com>
2024-05-25 14:56:51 -0400

    2.3.2-2 实现 upper

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -53,4 +53,40 @@ public class BinarySearch {
 
         return searchR(data, l, mid - 1, target);
     }
+
+    /**
+     * 2-2 实现 upper：大于 target 的最小值
+     * @param data
+     * @param target
+     * @return
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> int upper(E[] data, E target) {
+        // 在 data[l, r] 中寻找解，注意 r 也是闭区间
+        // r 的索引在取到 data.length 的时候，它不是数组data的一个合法索引
+        // 但是它是我们在这个问题，在我们这个定义下的合法解
+        int l = 0, r = data.length;
+
+        // 循环定义不是 l <= r，因为在我们定义的搜索范围中
+        // 一定有我们这个问题的解，所以当 l == r 碰上了，这一定是我们的解
+        // 这和我们之前二分查找法寻找一个target的解法不同之处，因为可能找到/找不到（有解/没解）
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (data[mid].compareTo(target) <= 0) { // 小细节 <=
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l; // 和return r一样的
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 1, 3, 3, 5, 5};
+        for (int i = 0; i <= 6; i++) {
+            System.out.print(BinarySearch.upper(arr, i) + " "); // 0 2 2 4 4 6 6
+        }
+        System.out.println();
+    }
+
 }
