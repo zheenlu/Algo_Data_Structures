f09c0e3bd4c2919b0137a850615976c00ac04d9b
zheenlu <lz.zhenlu@gmail.com>
2024-05-25 15:13:32 -0400

    2.3.2-3 二分查找法的变种：ceil

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -58,7 +58,7 @@ public class BinarySearch {
      * 2-2 实现 upper：大于 target 的最小值
      * @param data
      * @param target
-     * @return
+     * @return index of 大于 target 的最小值
      * @param <E>
      */
     public static <E extends Comparable<E>> int upper(E[] data, E target) {
@@ -81,10 +81,34 @@ public class BinarySearch {
         return l; // 和return r一样的
     }
 
+    /**
+     * 1. 先找到比target大的最小值，退后一位，如果退后一位位置上的数字就是target，
+     * 则返回索引即可（同时也满足”如果数组中存在元素，返回最大索引“）
+     * 2. 如果退后一位位置上的数字不是target，则返回比target大的最小值索引（即上一节讨论过的upper）
+     * @param data
+     * @param target
+     * @return
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> int ceil(E[] data, E target) {
+        int u = upper(data, target);
+        if (u - 1 >= 0 && data[u - 1] == target) {
+            return u - 1;
+        }
+        return u;
+    }
+
+
     public static void main(String[] args) {
+//        Integer[] arr = {1, 1, 3, 3, 5, 5};
+//        for (int i = 0; i <= 6; i++) {
+//            System.out.print(BinarySearch.upper(arr, i) + " "); // 0 2 2 4 4 6 6
+//        }
+//        System.out.println();
+
         Integer[] arr = {1, 1, 3, 3, 5, 5};
         for (int i = 0; i <= 6; i++) {
-            System.out.print(BinarySearch.upper(arr, i) + " "); // 0 2 2 4 4 6 6
+            System.out.print(BinarySearch.ceil(arr, i) + " "); // 0 1 2 3 4 5 6
         }
         System.out.println();
     }
