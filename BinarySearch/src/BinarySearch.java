1fc7d24877d056e1f79916e020812193f87d3e3b
zheenlu <lz.zhenlu@gmail.com>
2024-05-29 15:18:56 -0400

    2.3.2-6 二分查找法的变种：lower

--- BinarySearch/src/BinarySearch.java
+++ BinarySearch/src/BinarySearch.java
@@ -123,6 +123,26 @@ public class BinarySearch {
         return l;
     }
 
+    /**
+     * < target 的最大值索引
+     * @param data
+     * @param target
+     * @return
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> int lower(E[] data, E target) {
+        int l = -1, r = data.length - 1;
+        while (l < r) {
+            int mid = l + (r - l + 1) / 2;
+            if (data[mid].compareTo(target) < 0) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
 
     public static void main(String[] args) {
 //        Integer[] arr = {1, 1, 3, 3, 5, 5};
@@ -136,8 +156,10 @@ public class BinarySearch {
 //            System.out.print(BinarySearch.ceil(arr, i) + " "); // 0 1 2 3 4 5 6
 //        }
 //        System.out.println();
+//        for(int i = 0; i <= 6; i ++)
+//            System.out.print(BinarySearch.lower_ceil(arr, i) + " ");
         for(int i = 0; i <= 6; i ++)
-            System.out.print(BinarySearch.lower_ceil(arr, i) + " ");
+            System.out.print(BinarySearch.lower(arr, i) + " ");
         System.out.println();
     }
 
