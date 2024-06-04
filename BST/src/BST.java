5bee3d86ac9fca14ce3f63dff6ce431d53de46fc
zheenlu <lz.zhenlu@gmail.com>
2024-06-04 13:35:46 -0400

    2.4.1-3 向二分搜索树中添加元素

---
+++ BST/src/BST.java
@@ -0,0 +1,63 @@
+public class BST <E extends Comparable<E>> {
+    private class Node {
+        public E e;
+        public Node left, right;
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 用户调用的method
+     * 像二分搜索树中添加新的元素e
+     * @param e 新的元素e
+     */
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+
+    private void add(Node node, E e) {
+        // 递归终止条件有3种情况
+        if (e.equals(node.e)) {
+            // 我们的BTS设定中不包含重复元素，所以如果元素已存在，直接return
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+}
